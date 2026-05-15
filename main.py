503dd86e575f5c4754c30ecd2079615423055842
IlhamFaisalR23 <ilhamfaisalrr@gmail.com>
2026-05-15 16:00:46 +0700

    Lazy Loading BERT Python Fast API

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = None
 
 class RequestModel(BaseModel):
     text: str
 
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
 @app.post("/embedding")
 def get_embedding(req: RequestModel):
+    global model
+
+    if model is None:
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+
     embedding = model.encode(req.text).tolist()
     return {"embedding": embedding}
