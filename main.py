18f54b7ae3bdaa2bca9c6d6e7bae62b108a99e5f
IlhamFaisalR23 <ilhamfaisalrr@gmail.com>
2026-05-15 16:35:20 +0700

    Basic Example

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
-
-model = None
-
-class RequestModel(BaseModel):
-    text: str
 
 @app.get("/")
 def root():
     return {"status": "ok"}
-
-@app.post("/embedding")
-def get_embedding(req: RequestModel):
-    global model
-
-    if model is None:
-        model = SentenceTransformer('all-MiniLM-L6-v2')
-
-    embedding = model.encode(req.text).tolist()
-    return {"embedding": embedding}
